c9a835c84aa7d99297f5d17b629830d948ea8413
keshew <artyom.korotckov2017@yandex.ru>
2025-09-18 13:41:46 +0500

    Initial Commit

---
+++ Slots/SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsApp.swift
+//  Slots
+//
+//  Created by Артём Коротков on 18.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
